3aeb232b202f8378560fc84d0396e861c743c4f3
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-11-22 16:56:22 +0700

    Removed constant NAMELESS_PATH

--- www/index.php
+++ www/index.php
@@ -11,8 +11,6 @@ define('START_TIME',   microtime(TRUE));
 define('START_MEMORY', memory_get_usage());
 
 define('ROOT_PATH',        dirname(__DIR__) . DS);
-//TODO: remove this const
-define('NAMELESS_PATH',    ROOT_PATH . 'vendor' . DS . 'corpsee' . DS . 'nameless-source' . DS . 'src' . DS . 'Nameless' . DS);
 define('APPLICATION_PATH', ROOT_PATH . 'Application' . DS);
 define('CONFIG_PATH',      APPLICATION_PATH . 'configs' . DS);
 define('PUBLIC_PATH',      ROOT_PATH . 'www' . DS);
