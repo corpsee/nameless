4989e8685370d399e0193566dabb8983f0d08283
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-12-01 01:32:53 +0700

    Moved lib assets to libs

--- Application/Controllers/IndexController.php
+++ Application/Controllers/IndexController.php
@@ -15,14 +15,14 @@ class IndexController extends Controller
 
 		$styles = array
 		(
-			'/files/lib/bootstrap/2.3.2/css/bootstrap.css',
+			'/files/libs/bootstrap/2.3.2/css/bootstrap.css',
 			'/files/css/nameless.less',
 		);
 
 		$scripts = array
 		(
-			'/files/lib/jquery/1.10.2/jquery.js',
-			'/files/lib/bootstrap/2.3.2/js/bootstrap.js',
+			'/files/libs/jquery/1.10.2/jquery.js',
+			'/files/libs/bootstrap/2.3.2/js/bootstrap.js',
 		);
 
 		$data = array
