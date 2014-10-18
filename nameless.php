67149da36035a5a6047c0b5372eb6ef4252069c9
Dmitry Khomutov <poisoncorpsee@gmail.com>
2014-10-18 13:34:25 +0700

    Updated dependencies

--- nameless.php
+++ nameless.php
@@ -5,14 +5,14 @@ ini_set('display_errors', 1);
 
 define('DS', DIRECTORY_SEPARATOR);
 
-define('ROOT_PATH',        __DIR__ . DS);
+define('ROOT_PATH', __DIR__ . DS);
 define('APPLICATION_PATH', ROOT_PATH . 'Application' . DS);
-define('CONFIG_PATH',      APPLICATION_PATH . 'configs' . DS);
+define('CONFIG_PATH', APPLICATION_PATH . 'configs' . DS);
 
 define('PUBLIC_PATH', ROOT_PATH . 'www' . DS);
-define('FILE_PATH',   PUBLIC_PATH . 'files' . DS);
+define('FILE_PATH', PUBLIC_PATH . 'files' . DS);
 
-define('FILE_PATH_URL',   '/files/');
+define('FILE_PATH_URL', '/files/');
 
 require_once ROOT_PATH . 'vendor' . DS . 'autoload.php';
 
